4f497403d12595334cf799c12305bafc5b4a1577
ROllerozxa <rollerozxa@voxelmanip.se>
2024-01-26 21:46:59 +0100

    Reformat code

--- include/simple2d.h
+++ include/simple2d.h
@@ -10,33 +10,33 @@ extern "C" {
 
 // Apple
 #ifdef __APPLE__
-  #ifndef __TARGETCONDITIONALS__
-  #include "TargetConditionals.h"
-  #endif
-  #if TARGET_OS_OSX
-    #define MACOS true
-  #elif TARGET_OS_IOS
-    #define IOS   true
-  #elif TARGET_OS_TV
-    #define TVOS  true
-  #endif
+	#ifndef __TARGETCONDITIONALS__
+	#include "TargetConditionals.h"
+	#endif
+	#if TARGET_OS_OSX
+		#define MACOS true
+	#elif TARGET_OS_IOS
+		#define IOS   true
+	#elif TARGET_OS_TV
+		#define TVOS  true
+	#endif
 #endif
 
 // Windows
 #ifdef _WIN32
-  #define WINDOWS true
+	#define WINDOWS true
 #endif
 
 // Windows and MinGW
 #ifdef __MINGW32__
-  #define MINGW true
+	#define MINGW true
 #endif
 
 // GLES
 #if defined(__arm__) || IOS || TVOS
-  #define GLES true
+	#define GLES true
 #else
-  #define GLES false
+	#define GLES false
 #endif
 
 // Includes ////////////////////////////////////////////////////////////////////
@@ -47,58 +47,58 @@ extern "C" {
 #endif
 
 #if WINDOWS && !MINGW
-  #include <io.h>
-  #define  F_OK 0  // For testing file existence
+	#include <io.h>
+	#define  F_OK 0  // For testing file existence
 #else
-  #include <unistd.h>
+	#include <unistd.h>
 #endif
 
 #if WINDOWS
-  #include <stdio.h>
-  #include <math.h>
-  #include <windows.h>
-  // For terminal colors
-  #ifndef  ENABLE_VIRTUAL_TERMINAL_PROCESSING
-  #define  ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
-  #endif
+	#include <stdio.h>
+	#include <math.h>
+	#include <windows.h>
+	// For terminal colors
+	#ifndef  ENABLE_VIRTUAL_TERMINAL_PROCESSING
+	#define  ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+	#endif
 #endif
 
 // SDL
 #if IOS || TVOS
-  #include "SDL2/SDL.h"
+	#include "SDL2/SDL.h"
 #else
-  #include <SDL2/SDL.h>
+	#include <SDL2/SDL.h>
 #endif
 
 // If MinGW, undefine `main()` from SDL_main.c
 #if MINGW
-  #undef main
+	#undef main
 #endif
 
 // OpenGL
 #if GLES
-  #if IOS || TVOS
-    #include "SDL2/SDL_opengles2.h"
-  #else
-    #include <SDL2/SDL_opengles2.h>
-  #endif
+	#if IOS || TVOS
+		#include "SDL2/SDL_opengles2.h"
+	#else
+		#include <SDL2/SDL_opengles2.h>
+	#endif
 #else
-  #define GL_GLEXT_PROTOTYPES 1
-  #if WINDOWS
-    #include <glew.h>
-  #endif
-  #include <SDL2/SDL_opengl.h>
+	#define GL_GLEXT_PROTOTYPES 1
+	#if WINDOWS
+		#include <glew.h>
+	#endif
+	#include <SDL2/SDL_opengl.h>
 #endif
 
 // SDL libraries
 #if IOS || TVOS
-  #include "SDL2/SDL_image.h"
-  #include "SDL2/SDL_mixer.h"
-  #include "SDL2/SDL_ttf.h"
+	#include "SDL2/SDL_image.h"
+	#include "SDL2/SDL_mixer.h"
+	#include "SDL2/SDL_ttf.h"
 #else
-  #include <SDL2/SDL_image.h>
-  #include <SDL2/SDL_mixer.h>
-  #include <SDL2/SDL_ttf.h>
+	#include <SDL2/SDL_image.h>
+	#include <SDL2/SDL_mixer.h>
+	#include <SDL2/SDL_ttf.h>
 #endif
 
 // Simple 2D Definitions ///////////////////////////////////////////////////////
@@ -189,18 +189,18 @@ extern bool S2D_diagnostics;  // flag for whether to print diagnostics with S2D_
 
 // S2D_Event
 typedef struct {
-  int which;
-  int type;
-  int button;
-  bool dblclick;
-  const char *key;
-  int x;
-  int y;
-  int delta_x;
-  int delta_y;
-  int direction;
-  int axis;
-  int value;
+	int which;
+	int type;
+	int button;
+	bool dblclick;
+	const char *key;
+	int x;
+	int y;
+	int delta_x;
+	int delta_y;
+	int direction;
+	int axis;
+	int value;
 } S2D_Event;
 
 typedef void (*S2D_Update)();
@@ -211,136 +211,136 @@ typedef void (*S2D_On_Controller)(S2D_Event e);
 
 // S2D_GL_Point, for graphics calculations
 typedef struct {
-  GLfloat x;
-  GLfloat y;
+	GLfloat x;
+	GLfloat y;
 } S2D_GL_Point;
 
 // S2D_Color
 typedef struct {
-  GLfloat r;
-  GLfloat g;
-  GLfloat b;
-  GLfloat a;
+	GLfloat r;
+	GLfloat g;
+	GLfloat b;
+	GLfloat a;
 } S2D_Color;
 
 // S2D_Mouse
 typedef struct {
-  int visible;
-  int x;
-  int y;
+	int visible;
+	int x;
+	int y;
 } S2D_Mouse;
 
 // S2D_Viewport
 typedef struct {
-  int width;
-  int height;
-  int mode;
+	int width;
+	int height;
+	int mode;
 } S2D_Viewport;
 
 // S2D_Window
 typedef struct {
-  SDL_Window *sdl;
-  SDL_GLContext glcontext;
-  const GLubyte *S2D_GL_VENDOR;
-  const GLubyte *S2D_GL_RENDERER;
-  const GLubyte *S2D_GL_VERSION;
-  GLint S2D_GL_MAJOR_VERSION;
-  GLint S2D_GL_MINOR_VERSION;
-  const GLubyte *S2D_GL_SHADING_LANGUAGE_VERSION;
-  const char *title;
-  int width;
-  int height;
-  int orig_width;
-  int orig_height;
-  S2D_Viewport viewport;
-  S2D_Update update;
-  S2D_Render render;
-  int flags;
-  S2D_Mouse mouse;
-  S2D_On_Key on_key;
-  S2D_On_Mouse on_mouse;
-  S2D_On_Controller on_controller;
-  bool vsync;
-  int fps_cap;
-  S2D_Color background;
-  const char *icon;
-  Uint32 frames;
-  Uint32 elapsed_ms;
-  Uint32 loop_ms;
-  Uint32 delay_ms;
-  double fps;
-  bool close;
+	SDL_Window *sdl;
+	SDL_GLContext glcontext;
+	const GLubyte *S2D_GL_VENDOR;
+	const GLubyte *S2D_GL_RENDERER;
+	const GLubyte *S2D_GL_VERSION;
+	GLint S2D_GL_MAJOR_VERSION;
+	GLint S2D_GL_MINOR_VERSION;
+	const GLubyte *S2D_GL_SHADING_LANGUAGE_VERSION;
+	const char *title;
+	int width;
+	int height;
+	int orig_width;
+	int orig_height;
+	S2D_Viewport viewport;
+	S2D_Update update;
+	S2D_Render render;
+	int flags;
+	S2D_Mouse mouse;
+	S2D_On_Key on_key;
+	S2D_On_Mouse on_mouse;
+	S2D_On_Controller on_controller;
+	bool vsync;
+	int fps_cap;
+	S2D_Color background;
+	const char *icon;
+	Uint32 frames;
+	Uint32 elapsed_ms;
+	Uint32 loop_ms;
+	Uint32 delay_ms;
+	double fps;
+	bool close;
 } S2D_Window;
 
 // S2D_Image
 typedef struct {
-  const char *path;
-  SDL_Surface *surface;
-  int format;
-  GLuint texture_id;
-  S2D_Color color;
-  int x;
-  int y;
-  int width;
-  int height;
-  int orig_width;
-  int orig_height;
-  GLfloat rotate;  // Rotation angle in degrees
-  GLfloat rx;      // X coordinate to be rotated around
-  GLfloat ry;      // Y coordinate to be rotated around
+	const char *path;
+	SDL_Surface *surface;
+	int format;
+	GLuint texture_id;
+	S2D_Color color;
+	int x;
+	int y;
+	int width;
+	int height;
+	int orig_width;
+	int orig_height;
+	GLfloat rotate;  // Rotation angle in degrees
+	GLfloat rx;      // X coordinate to be rotated around
+	GLfloat ry;      // Y coordinate to be rotated around
 } S2D_Image;
 
 // S2D_Sprite
 typedef struct {
-  const char *path;
-  S2D_Image *img;
-  S2D_Color color;
-  int x;
-  int y;
-  int width;
-  int height;
-  int clip_width;
-  int clip_height;
-  GLfloat rotate;  // Rotation angle in degrees
-  GLfloat rx;      // X coordinate to be rotated around
-  GLfloat ry;      // Y coordinate to be rotated around
-  GLfloat tx1;
-  GLfloat ty1;
-  GLfloat tx2;
-  GLfloat ty2;
-  GLfloat tx3;
-  GLfloat ty3;
-  GLfloat tx4;
-  GLfloat ty4;
+	const char *path;
+	S2D_Image *img;
+	S2D_Color color;
+	int x;
+	int y;
+	int width;
+	int height;
+	int clip_width;
+	int clip_height;
+	GLfloat rotate;  // Rotation angle in degrees
+	GLfloat rx;      // X coordinate to be rotated around
+	GLfloat ry;      // Y coordinate to be rotated around
+	GLfloat tx1;
+	GLfloat ty1;
+	GLfloat tx2;
+	GLfloat ty2;
+	GLfloat tx3;
+	GLfloat ty3;
+	GLfloat tx4;
+	GLfloat ty4;
 } S2D_Sprite;
 
 // S2D_Text
 typedef struct {
-  const char *font;
-  SDL_Surface *surface;
-  GLuint texture_id;
-  TTF_Font *font_data;
-  S2D_Color color;
-  char *msg;
-  int x;
-  int y;
-  int width;
-  int height;
-  GLfloat rotate;  // Rotation angle in degrees
-  GLfloat rx;      // X coordinate to be rotated around
-  GLfloat ry;      // Y coordinate to be rotated around
+	const char *font;
+	SDL_Surface *surface;
+	GLuint texture_id;
+	TTF_Font *font_data;
+	S2D_Color color;
+	char *msg;
+	int x;
+	int y;
+	int width;
+	int height;
+	GLfloat rotate;  // Rotation angle in degrees
+	GLfloat rx;      // X coordinate to be rotated around
+	GLfloat ry;      // Y coordinate to be rotated around
 } S2D_Text;
 
 // S2D_Sound
 typedef struct {
-  const char *path;
-  Mix_Chunk *data;
+	const char *path;
+	Mix_Chunk *data;
 } S2D_Sound;
 
 // S2D_Music
 typedef struct {
-  const char *path;
-  Mix_Music *data;
+	const char *path;
+	Mix_Music *data;
 } S2D_Music;
 
 // Simple 2D Functions /////////////////////////////////////////////////////////
@@ -371,8 +371,8 @@ void S2D_Diagnostics(bool status);
 void S2D_Windows_EnableTerminalColors();
 
 /*
-* Initialize Simple 2D subsystems
-*/
+ * Initialize Simple 2D subsystems
+ */
 bool S2D_Init();
 
 /*
@@ -406,46 +406,46 @@ S2D_GL_Point S2D_GetRectRotationPoint(int x, int y, int w, int h, int position);
  * Draw a triangle
  */
 void S2D_DrawTriangle(
-  GLfloat x1, GLfloat y1,
-  GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-  GLfloat x2, GLfloat y2,
-  GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-  GLfloat x3, GLfloat y3,
-  GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3
+	GLfloat x1, GLfloat y1,
+	GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+	GLfloat x2, GLfloat y2,
+	GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+	GLfloat x3, GLfloat y3,
+	GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3
 );
 
 /*
  * Draw a quad, using two triangles
  */
 void S2D_DrawQuad(
-  GLfloat x1, GLfloat y1,
-  GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-  GLfloat x2, GLfloat y2,
-  GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-  GLfloat x3, GLfloat y3,
-  GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3,
-  GLfloat x4, GLfloat y4,
-  GLfloat r4, GLfloat g4, GLfloat b4, GLfloat a4
+	GLfloat x1, GLfloat y1,
+	GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+	GLfloat x2, GLfloat y2,
+	GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+	GLfloat x3, GLfloat y3,
+	GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3,
+	GLfloat x4, GLfloat y4,
+	GLfloat r4, GLfloat g4, GLfloat b4, GLfloat a4
 );
 
 /*
  * Draw a line from a quad
  */
 void S2D_DrawLine(
-  GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2,
-  GLfloat width,
-  GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-  GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-  GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3,
-  GLfloat r4, GLfloat g4, GLfloat b4, GLfloat a4
+	GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2,
+	GLfloat width,
+	GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+	GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+	GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3,
+	GLfloat r4, GLfloat g4, GLfloat b4, GLfloat a4
 );
 
 /*
  * Draw a circle from triangles
  */
 void S2D_DrawCircle(
-  GLfloat x, GLfloat y, GLfloat radius, int sectors,
-  GLfloat r, GLfloat g, GLfloat b, GLfloat a
+	GLfloat x, GLfloat y, GLfloat radius, int sectors,
+	GLfloat r, GLfloat g, GLfloat b, GLfloat a
 );
 
 // Image ///////////////////////////////////////////////////////////////////////
@@ -505,8 +505,8 @@ void S2D_FreeSprite(S2D_Sprite *spr);
 S2D_Text *S2D_CreateText(const char *font, const char *msg, int size);
 
 /*
-* Set the text message
-*/
+ * Set the text message
+ */
 void S2D_SetText(S2D_Text *txt, const char *msg, ...);
 
 /*
@@ -653,7 +653,7 @@ void S2D_OpenControllers();
  * Create a window
  */
 S2D_Window *S2D_CreateWindow(
-  const char *title, int width, int height, S2D_Update, S2D_Render, int flags
+	const char *title, int width, int height, S2D_Update, S2D_Render, int flags
 );
 
 /*
@@ -692,16 +692,16 @@ int S2D_GL_CheckLinked(GLuint program, char *name);
 void S2D_GL_GetViewportScale(S2D_Window *window, int *w, int *h, double *scale);
 void S2D_GL_SetViewport(S2D_Window *window);
 void S2D_GL_CreateTexture(
-  GLuint *id, GLint format,
-  int w, int h,
-  const GLvoid *data, GLint filter);
+	GLuint *id, GLint format,
+	int w, int h,
+	const GLvoid *data, GLint filter);
 void S2D_GL_DrawTriangle(
-  GLfloat x1, GLfloat y1,
-  GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-  GLfloat x2, GLfloat y2,
-  GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-  GLfloat x3, GLfloat y3,
-  GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3);
+	GLfloat x1, GLfloat y1,
+	GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+	GLfloat x2, GLfloat y2,
+	GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+	GLfloat x3, GLfloat y3,
+	GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3);
 void S2D_GL_DrawImage(S2D_Image *img);
 void S2D_GL_DrawSprite(S2D_Sprite *spr);
 void S2D_GL_DrawText(S2D_Text *txt);
@@ -712,44 +712,44 @@ void S2D_GL_FlushBuffers();
 // OpenGL & GLES Internal Functions ////////////////////////////////////////////
 
 #if GLES
-  int S2D_GLES_Init();
-  void S2D_GLES_ApplyProjection(GLfloat orthoMatrix[16]);
-  void S2D_GLES_DrawTriangle(
-    GLfloat x1, GLfloat y1,
-    GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-    GLfloat x2, GLfloat y2,
-    GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-    GLfloat x3, GLfloat y3,
-    GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3);
-  void S2D_GLES_DrawImage(S2D_Image *img);
-  void S2D_GLES_DrawSprite(S2D_Sprite *spr);
-  void S2D_GLES_DrawText(S2D_Text *txt);
+	int S2D_GLES_Init();
+	void S2D_GLES_ApplyProjection(GLfloat orthoMatrix[16]);
+	void S2D_GLES_DrawTriangle(
+		GLfloat x1, GLfloat y1,
+		GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+		GLfloat x2, GLfloat y2,
+		GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+		GLfloat x3, GLfloat y3,
+		GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3);
+	void S2D_GLES_DrawImage(S2D_Image *img);
+	void S2D_GLES_DrawSprite(S2D_Sprite *spr);
+	void S2D_GLES_DrawText(S2D_Text *txt);
 #else
-  int S2D_GL2_Init();
-  int S2D_GL3_Init();
-  void S2D_GL2_ApplyProjection(int w, int h);
-  void S2D_GL3_ApplyProjection(GLfloat orthoMatrix[16]);
-  void S2D_GL2_DrawTriangle(
-    GLfloat x1, GLfloat y1,
-    GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-    GLfloat x2, GLfloat y2,
-    GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-    GLfloat x3, GLfloat y3,
-    GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3);
-  void S2D_GL3_DrawTriangle(
-    GLfloat x1, GLfloat y1,
-    GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-    GLfloat x2, GLfloat y2,
-    GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-    GLfloat x3, GLfloat y3,
-    GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3);
-  void S2D_GL2_DrawImage(S2D_Image *img);
-  void S2D_GL3_DrawImage(S2D_Image *img);
-  void S2D_GL2_DrawSprite(S2D_Sprite *spr);
-  void S2D_GL3_DrawSprite(S2D_Sprite *spr);
-  void S2D_GL2_DrawText(S2D_Text *txt);
-  void S2D_GL3_DrawText(S2D_Text *txt);
-  void S2D_GL3_FlushBuffers();
+	int S2D_GL2_Init();
+	int S2D_GL3_Init();
+	void S2D_GL2_ApplyProjection(int w, int h);
+	void S2D_GL3_ApplyProjection(GLfloat orthoMatrix[16]);
+	void S2D_GL2_DrawTriangle(
+		GLfloat x1, GLfloat y1,
+		GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+		GLfloat x2, GLfloat y2,
+		GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+		GLfloat x3, GLfloat y3,
+		GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3);
+	void S2D_GL3_DrawTriangle(
+		GLfloat x1, GLfloat y1,
+		GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+		GLfloat x2, GLfloat y2,
+		GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+		GLfloat x3, GLfloat y3,
+		GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3);
+	void S2D_GL2_DrawImage(S2D_Image *img);
+	void S2D_GL3_DrawImage(S2D_Image *img);
+	void S2D_GL2_DrawSprite(S2D_Sprite *spr);
+	void S2D_GL3_DrawSprite(S2D_Sprite *spr);
+	void S2D_GL2_DrawText(S2D_Text *txt);
+	void S2D_GL3_DrawText(S2D_Text *txt);
+	void S2D_GL3_FlushBuffers();
 #endif
 
 #ifdef __cplusplus
--- src/shapes.c
+++ src/shapes.c
@@ -2,7 +2,6 @@
 
 #include "../include/simple2d.h"
 
-
 /*
  * Rotate a point around a given point
  * Params:
@@ -13,142 +12,137 @@
  */
 S2D_GL_Point S2D_RotatePoint(S2D_GL_Point p, GLfloat angle, GLfloat rx, GLfloat ry) {
 
-  // Convert from degrees to radians
-  angle = angle * M_PI / 180.0;
+	// Convert from degrees to radians
+	angle = angle * M_PI / 180.0;
 
-  // Get the sine and cosine of the angle
-  GLfloat sa = sin(angle);
-  GLfloat ca = cos(angle);
+	// Get the sine and cosine of the angle
+	GLfloat sa = sin(angle);
+	GLfloat ca = cos(angle);
 
-  // Translate point to origin
-  p.x -= rx;
-  p.y -= ry;
+	// Translate point to origin
+	p.x -= rx;
+	p.y -= ry;
 
-  // Rotate point
-  GLfloat xnew = p.x * ca - p.y * sa;
-  GLfloat ynew = p.x * sa + p.y * ca;
+	// Rotate point
+	GLfloat xnew = p.x * ca - p.y * sa;
+	GLfloat ynew = p.x * sa + p.y * ca;
 
-  // Translate point back
-  p.x = xnew + rx;
-  p.y = ynew + ry;
+	// Translate point back
+	p.x = xnew + rx;
+	p.y = ynew + ry;
 
-  return p;
+	return p;
 }
-
 
 /*
  * Get the point to be rotated around given a position in a rectangle
  */
 S2D_GL_Point S2D_GetRectRotationPoint(int x, int y, int w, int h, int position) {
 
-  S2D_GL_Point p;
+	S2D_GL_Point p;
 
-  switch (position) {
-    case S2D_CENTER:
-      p.x = x + (w / 2.0);
-      p.y = y + (h / 2.0);
-      break;
-    case S2D_TOP_LEFT:
-      p.x = x;
-      p.y = y;
-      break;
-    case S2D_TOP_RIGHT:
-      p.x = x + w;
-      p.y = y;
-      break;
-    case S2D_BOTTOM_LEFT:
-      p.x = x;
-      p.y = y + h;
-      break;
-    case S2D_BOTTOM_RIGHT:
-      p.x = x + w;
-      p.y = y + h;
-      break;
-  }
+	switch (position) {
+		case S2D_CENTER:
+			p.x = x + (w / 2.0);
+			p.y = y + (h / 2.0);
+			break;
+		case S2D_TOP_LEFT:
+			p.x = x;
+			p.y = y;
+			break;
+		case S2D_TOP_RIGHT:
+			p.x = x + w;
+			p.y = y;
+			break;
+		case S2D_BOTTOM_LEFT:
+			p.x = x;
+			p.y = y + h;
+			break;
+		case S2D_BOTTOM_RIGHT:
+			p.x = x + w;
+			p.y = y + h;
+			break;
+	}
 
-  return p;
+	return p;
 }
-
 
 /*
  * Draw a triangle
  */
 void S2D_DrawTriangle(GLfloat x1, GLfloat y1,
-                      GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-                      GLfloat x2, GLfloat y2,
-                      GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-                      GLfloat x3, GLfloat y3,
-                      GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3) {
+					  GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+					  GLfloat x2, GLfloat y2,
+					  GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+					  GLfloat x3, GLfloat y3,
+					  GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3) {
 
-  S2D_GL_DrawTriangle(x1, y1, r1, g1, b1, a1,
-                      x2, y2, r2, g2, b2, a2,
-                      x3, y3, r3, g3, b3, a3);
+	S2D_GL_DrawTriangle(x1, y1, r1, g1, b1, a1,
+						x2, y2, r2, g2, b2, a2,
+						x3, y3, r3, g3, b3, a3);
 }
-
 
 /*
  * Draw a quad, using two triangles
  */
 void S2D_DrawQuad(GLfloat x1, GLfloat y1,
-                  GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-                  GLfloat x2, GLfloat y2,
-                  GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-                  GLfloat x3, GLfloat y3,
-                  GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3,
-                  GLfloat x4, GLfloat y4,
-                  GLfloat r4, GLfloat g4, GLfloat b4, GLfloat a4) {
+				  GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+				  GLfloat x2, GLfloat y2,
+				  GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+				  GLfloat x3, GLfloat y3,
+				  GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3,
+				  GLfloat x4, GLfloat y4,
+				  GLfloat r4, GLfloat g4, GLfloat b4, GLfloat a4) {
 
-  S2D_GL_DrawTriangle(x1, y1, r1, g1, b1, a1,
-                      x2, y2, r2, g2, b2, a2,
-                      x3, y3, r3, g3, b3, a3);
+	S2D_GL_DrawTriangle(x1, y1, r1, g1, b1, a1,
+						x2, y2, r2, g2, b2, a2,
+						x3, y3, r3, g3, b3, a3);
 
-  S2D_GL_DrawTriangle(x3, y3, r3, g3, b3, a3,
-                      x4, y4, r4, g4, b4, a4,
-                      x1, y1, r1, g1, b1, a1);
+	S2D_GL_DrawTriangle(x3, y3, r3, g3, b3, a3,
+						x4, y4, r4, g4, b4, a4,
+						x1, y1, r1, g1, b1, a1);
 };
-
 
 /*
  * Draw a line from a quad
  */
 void S2D_DrawLine(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2,
-                  GLfloat width,
-                  GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
-                  GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
-                  GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3,
-                  GLfloat r4, GLfloat g4, GLfloat b4, GLfloat a4) {
+				  GLfloat width,
+				  GLfloat r1, GLfloat g1, GLfloat b1, GLfloat a1,
+				  GLfloat r2, GLfloat g2, GLfloat b2, GLfloat a2,
+				  GLfloat r3, GLfloat g3, GLfloat b3, GLfloat a3,
+				  GLfloat r4, GLfloat g4, GLfloat b4, GLfloat a4) {
 
-  double length = sqrt(powf(x1 - x2, 2) + powf(y1 - y2, 2));
-  double x = ((x2 - x1) / length) * width / 2;
-  double y = ((y2 - y1) / length) * width / 2;
+	double length = sqrt(powf(x1 - x2, 2) + powf(y1 - y2, 2));
+	double x = ((x2 - x1) / length) * width / 2;
+	double y = ((y2 - y1) / length) * width / 2;
 
-  S2D_DrawQuad(
-    x1 - y, y1 + x, r1, g1, b1, a1,
-    x1 + y, y1 - x, r2, g2, b2, a2,
-    x2 + y, y2 - x, r3, g3, b3, a3,
-    x2 - y, y2 + x, r4, g4, b4, a4
-  );
+	S2D_DrawQuad(
+		x1 - y, y1 + x, r1, g1, b1, a1,
+		x1 + y, y1 - x, r2, g2, b2, a2,
+		x2 + y, y2 - x, r3, g3, b3, a3,
+		x2 - y, y2 + x, r4, g4, b4, a4
+	);
 };
-
 
 /*
  * Draw a circle from triangles
  */
 void S2D_DrawCircle(GLfloat x, GLfloat y, GLfloat radius, int sectors,
-                    GLfloat r, GLfloat g, GLfloat b, GLfloat a) {
+					GLfloat r, GLfloat g, GLfloat b, GLfloat a) {
 
-  double angle = 2 * M_PI / sectors;
+	double angle = 2 * M_PI / sectors;
 
-  for (int i = 0; i < sectors; i++) {
+	for (int i = 0; i < sectors; i++) {
 
-    GLfloat x1 = x + radius * cos(i * angle);
-    GLfloat y1 = y + radius * sin(i * angle);
+		GLfloat x1 = x + radius * cos(i * angle);
+		GLfloat y1 = y + radius * sin(i * angle);
 
-    GLfloat x2 = x + radius * cos((i - 1) * angle);
-    GLfloat y2 = y + radius * sin((i - 1) * angle);
+		GLfloat x2 = x + radius * cos((i - 1) * angle);
+		GLfloat y2 = y + radius * sin((i - 1) * angle);
 
-    S2D_GL_DrawTriangle( x,  y, r, g, b, a,
-                        x1, y1, r, g, b, a,
-                        x2, y2, r, g, b, a);
-  }
+		S2D_GL_DrawTriangle( x,  y, r, g, b, a,
+							x1, y1, r, g, b, a,
+							x2, y2, r, g, b, a);
+	}
 }
